2af59bfedab7f0baad87a5b1d2fe44ac29e5c43d
Cyranowski, Mariusz (EXT) <mariusz.cyranowski@skanska.pl>
2015-11-05 16:27:00 +0100

    dodaje projekt konwersji modelu musicstore do JSON

---
+++ MusicStoreModel/ModelToJson/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicStore.Models;
+using Newtonsoft.Json;
+
+namespace ModelToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var albums = SampleData.GetAlbums(string.Empty, SampleData.Genres, SampleData.Artists);
+            var json = JsonConvert.SerializeObject(albums);
+            Console.Write(json);
+        }
+    }
+}
